8da1902154b723804d41016441212b4e4c47e162
epm <w783441698@gmail.com>
2022-10-21 16:51:01 +0800

    quasi_newton DONE

--- quasi_newton.py
+++ quasi_newton.py
@@ -1,101 +1,95 @@
 import numpy as np
 import sympy as sp
 import math
+from newton import newton
 
-def quasi_newton(f, x0):
-    i = 0
-    x = []
-    H = []
-    x_num = []
+def quasi_newton(F, x1, x2, x0, epsilon):
+    g = np.array([[sp.diff(F, x1)],
+                  [sp.diff(F, x2)]])
+    h = np.array([[sp.diff(F, x1, x1), sp.diff(F, x1, x2)],
+                  [sp.diff(F, x2, x1), sp.diff(F, x2, x2)]])
+    print("g:\n", g)
+    print("h:\n", h)
     gs = []
-    ts = []
+    hs = []
+    f = []
+    p = []
+    x = []
+    x_num = []
     t = sp.symbols("t")
     t_num = []
-    p = []
+    x.append(x0)
+    x_num.append(x0)
+    hs.append(np.array([[1, 0], 
+                        [0, 1]], dtype = "float64"))
+    gs.append(np.array([
+                      [float(g[0][0].evalf(subs={"x1":x_num[0][0][0], "x2":x_num[0][1][0]}))],
+                      [float(g[1][0].evalf(subs={"x1":x_num[0][0][0], "x2":x_num[0][1][0]}))],
+                            ]))
+    f.append(float(F.evalf(subs={"x1":x[0][0][0], "x2":x[0][1][0]})))
     delta_x = []
     delta_g = []
-    while i == 0 or math.sqrt(g(f, x_num[i])[0][0] ** 2 + g(f, x_num[i])[1][0] ** 2) >= 0.01:
+    i = 0
+    while i == 0 or math.sqrt(gs[i][0][0] ** 2 + gs[i][1][0] ** 2) >= 0.001 :
         if i == 0:
             print("Roud %d"%(i + 1))
-            H.append(np.array([[1, 0],[0, 1]]))
-            print("H%d:\n"%(i),np.around( H[i], 6))
-            x_num.append(np.array(x0))
-            x.append(np.array(x0))
-            print("x0:\n", np.around(x_num[i], 6))
-            gs.append(g(f, x_num[i]))
-            p.append(-gs[i])
-            print("g0:\n", np.around(gs[i], 6))
-            print("p0:\n", np.around(p[i], 6))
-            x.append(x[i] + t * p[0])
-            print("x%d:\n"%(i + 1), x[i+1])
-            print("f(x%d):\n"%(i + 1), f(x[i+1]))
-            print("f'(t):\n", sp.diff(f(x[i+1]), t))
-            t_num.append(np.array(sp.solve(sp.diff(f(x[i+1]), t), t)))
-            print(type(t_num[i]))
-            print(t_num[i])
-            print("t0:\n", t_num[i])
-            x_num.append(x[i] + t_num[i] * p[i])
-            print("x1:\n", x_num[i+1])
-            #gs.append(g(x_num[i+1]))
+            p.append(- gs[i])
+            x.append(x[i] + t * p[i])
+            f.append(F.evalf(subs = {"x1":x[i + 1][0][0], "x2":x[i + 1][1][0]}))
+            t_num.append(newton(f[i + 1], t, 1, 0.001))
+            x_num.append(np.array([
+                [x[i + 1][0][0].evalf(subs = {"t": t_num[i]})],
+                [x[i + 1][1][0].evalf(subs = {"t": t_num[i]})]
+                ]))
+            gs.append(np.array([
+                [g[0][0].evalf(subs = {"x1":x_num[i + 1][0][0], "x2":x_num[i + 1][1][0]})],
+                [g[1][0].evalf(subs = {"x1":x_num[i + 1][0][0], "x2":x_num[i + 1][1][0]})]
+                ]))
+            print("x%d:\n"%(i), x_num[i])
+            print("g%d:\n"%(i), gs[i])
+            print("p%d:\n"%(i), p[i])
+            print("x%d:\n"%(i + 1), x[i + 1])
+            print("f%d:\n"%(i + 1), f[i + 1])
+            print("t%d:\n"%(i), t_num[i])
+            print("x%d:\n"%(i + 1), x_num[i + 1])
             print("-" * 100)
         else:
             print("Roud %d"%(i + 1))
-            gs.append(g(f, x_num[i]))
             delta_x.append(x_num[i] - x_num[i - 1])
             delta_g.append(gs[i] - gs[i - 1])
-            add1 = delta_x[i - 1] @ delta_x[i - 1].T / (delta_x[i - 1].T @ delta_g[i - 1])
-            add2 = (H[i - 1] @ delta_g[i - 1] @ delta_g[i - 1].T @ H[i - 1] / 
-                    (delta_g[i - 1].T @ H[i - 1] @ delta_g[i - 1]))
-            H.append(H[i - 1] + add1 - add2)
-            print("g:\n", gs[i])
-            print("delta_x:\n", delta_x[i - 1])
-            print("delta_g:\n", delta_g[i - 1])
-            print("H%d:\n"%(i), H[i])
-            p.append(- H[i] @ gs[i])
-            print("p%d:\n"%(i), p[i])
+            add1 = (delta_x[i - 1] @ delta_x[i - 1].T) / (delta_x[i - 1].T @ delta_g[i - 1])
+            add2 = ((hs[i - 1] @ delta_g[i - 1] @ delta_g[i - 1].T @ hs[i - 1]) 
+                  / (delta_g[i - 1].T @ hs[i - 1] @ delta_g[i - 1]))
+            hs.append(hs[i - 1] + add1 - add2)
+            p.append(- hs[i] @ gs[i])
             x.append(x_num[i] + t * p[i])
+            f.append(F.evalf(subs = {"x1":x[i + 1][0][0], "x2":x[i + 1][1][0]}))
+            t_num.append(newton(f[i + 1], t, 1, 0.001))
+            x_num.append(np.array([
+                [x[i + 1][0][0].evalf(subs = {"t": t_num[i]})],
+                [x[i + 1][1][0].evalf(subs = {"t": t_num[i]})]
+                ]))
+            gs.append(np.array([
+                [g[0][0].evalf(subs = {"x1":x_num[i + 1][0][0], "x2":x_num[i + 1][1][0]})],
+                [g[1][0].evalf(subs = {"x1":x_num[i + 1][0][0], "x2":x_num[i + 1][1][0]})]
+                ]))
+            print("x%d:\n"%(i), x_num[i])
+            print("g%d:\n"%(i), gs[i])
+            print("p%d:\n"%(i), p[i])
             print("x%d:\n"%(i + 1), x[i + 1])
-            t_num.append(sp.solve(sp.diff(f(x[i + 1]), t), t))
-            print("f'(t):\n", sp.diff(f(x[i+1]), t))
-            print("t%d:\n"%(i + 1), t_num[i])
-            x_num.append(x_num[i] + t_num[i] *  p[i])
+            print("f%d:\n"%(i + 1), f[i + 1])
+            print("t%d:\n"%(i), t_num[i])
             print("x%d:\n"%(i + 1), x_num[i + 1])
             print("-" * 100)
         i += 1
 
-    # The second round
-#    delta_x_0 = x_1_num - x_0
-#    delta_g_0 = g_1 - g_0
-#    print("delta_x_0\n", delta_x_0)
-#    print("delta_g_0\n", delta_g_0)
-#    H_0 = np.array([[1,0], [0, 1]])
-#    print(delta_x_0 @ delta_x_0.T)
-#    print(delta_x_0.T @ delta_g_0)
-#    add1 = delta_x_0 @ delta_x_0.T / (delta_x_0.T @ delta_g_0)
-#    print(add1)
-#    add2 = (H_0 @ delta_g_0 @ delta_g_0.T @ H_0) / delta_g_0.T @ H_0 @ delta_g_0
-#    print(add2)
-#    H_1 = H_0 + add1 - add2
-#    print(H_1)
-#    p_1 = - H_1 @ g_1
-#    print(p_1)
-#    x_2 = x_1_num + t * p_1
-#    print(x_2)
-#    t_num = sp.solve(sp.diff(f(x_2), t), t)
-#    print(sp.diff(f(x_2), t))
-#    print(t_num)
-#    x_2_num = x_1_num + t_num * p_1
-#    print(x_2_num)
-
-
-
-
-def f(X):
-    return X[0][0] ** 2 + 4 * X[1][0] ** 2
-
-def g(f, X):
-    return np.gradient(f, X)
+def test():
+    x1 = sp.symbols("x1")
+    x2 = sp.symbols("x2")
+    F = x1 ** 2 + 25 * x2 ** 2
+    x0 = np.array([[2], [2]])
+    quasi_newton(F, x1, x2, x0, 0.001)
 
 if __name__ == "__main__":
-    quasi_newton(f, [[1], [1]])
+    test()
 
